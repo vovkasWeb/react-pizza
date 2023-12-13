7984bd705c4120bf91c1af3b36830f1d21e68289
vovkasWeb <vova77557@gmail.com>
2023-12-13 15:23:59 +0200

    add: routers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './scss/app.scss'
-import App from './App';
+import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <App />
-);
-
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+)
