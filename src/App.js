9ff3a4b964754025d397f9907c83b23e87c17640
vovkasWeb <vova77557@gmail.com>
2023-12-14 23:28:02 +0200

    add: json and pizza configuration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { useEffect, useState } from 'react'
 import { Header } from './components'
 import { Home, Cart } from './pages'
 
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
-	// const clickPoKnopke = () => {
-	// 	alert('Hello!')
-	// }
+	const [pizzas, setPizzas] = useState([])
+	useEffect(() => {
+		fetch('/db.json')
+			.then(res => res.json())
+			.then(json => setPizzas(json.pizzas))
+			.catch(e => console.error(e))
+	}, [])
 
 	return (
 		<div className='wrapper'>
@@ -14,7 +19,7 @@ function App() {
 
 			<div className='content'>
 				<Routes>
-					<Route path='/' element={<Home />} />
+					<Route path='/' element={<Home pizzas={pizzas} />} />
 					<Route path='/cart' element={<Cart />} />
 				</Routes>
 			</div>
